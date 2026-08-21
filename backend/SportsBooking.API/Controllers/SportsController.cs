582e7d6fa799250c085d5e4787a53cb6fbea34bc
mraznyrazeek <mraznyrazeek@gmail.com>
2026-08-21 15:37:53 +0530

    GET Controller added for Sport

---
+++ backend/SportsBooking.API/Controllers/SportsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsBooking.API.Models;
+
+namespace SportsBooking.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SportsController : ControllerBase
+    {
+        private readonly SportsBookingDbContext _context;
+
+        public SportsController(SportsBookingDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sports
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sport>>> GetSports()
+        {
+            return await _context.Sports.ToListAsync();
+        }
+
+        // GET: api/Sports/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sport>> GetSport(int id)
+        {
+            var sport = await _context.Sports.FindAsync(id);
+
+            if (sport == null)
+            {
+                return NotFound();
+            }
+
+            return sport;
+        }
+    }
+}
--- backend/SportsBooking.API/Models/SportsBookingDbContext.cs
+++ backend/SportsBooking.API/Models/SportsBookingDbContext.cs
@@ -29,10 +29,6 @@ public partial class SportsBookingDbContext : DbContext
 
     public virtual DbSet<Sport> Sports { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseOracle("User Id=SPORTS_BOOKING;Password=SportsBooking123;Data Source=localhost:1521/XEPDB1;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
